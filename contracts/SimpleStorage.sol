457e1d63729a45d2aee01a2e4f9c82f3486d2832
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-22 23:07:22 +0530

    minor fix in cotract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.7;
+pragma solidity ^0.7.3;
 
 contract SimpleStorage {
     uint256 favNumber;
@@ -7,13 +7,13 @@ contract SimpleStorage {
         uint256 indexed newNumber,
         uint256 addedNumber,
         address sender
-    )
+    );
 
     function store(uint256 newFavNumber) public {
         emit storedNumber(
             favNumber,
             newFavNumber,
-            favNumber + newFavnumber,
+            favNumber + newFavNumber,
             msg.sender
         );
         favNumber = newFavNumber;
